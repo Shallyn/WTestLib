ace92ddc75f89db46e787641ba6f3713427db74e
Shallyn <shallyn.liu@foxmail.com>
2019-07-09 15:13:16 +0800

    7_9 update

--- strain/exe.py
+++ strain/exe.py
@@ -251,7 +251,8 @@ def event_scan(gps, sH1, sL1, sV1,
     
     # Step.5 Plot snr q scan spectrum.
     for data in sLIST:
-        print(data.sigma2)
+        print(data.ifo)
+        print(data)
         func = snr_q_scanf(data.value, tmpl.template, 
                            data.fs, data.epoch, 
                            cut=None, psd = data.psdfun_setted,

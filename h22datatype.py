f14f4ead1e5dbc9bbabc5807589d654b75977451
Shallyn <shallyn.liu@foxmail.com>
2020-06-28 14:06:06 +0800

    update

--- h22datatype.py
+++ h22datatype.py
@@ -38,7 +38,7 @@ def get_fmin(fdimless, Mtotal):
     return fdimless * pow(c_SI, 3) / (G_SI * M_Sun_SI * Mtotal)
 
 def get_fini_dimless(fmin, Mtotal):
-    return fmin * G_SI * Mtotal / pow(c_SI, 3)
+    return fmin * G_SI * Mtotal * M_Sun_SI / pow(c_SI, 3)
 
 def dim_h(r, M):
     return r * pc_SI * 1e6 * c_SI**2 / ( M * M_Sun_SI * G_SI )

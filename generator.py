520cd8a5b0fdff9cb2d86fb9d5bdaa809a5d9ed1
Shallyn <shallyn.liu@foxmail.com>
2020-10-21 21:07:48 +0800

    update

--- generator.py
+++ generator.py
@@ -415,7 +415,7 @@ class Generator(object):
         else:
             return cev
 
-
+from .h22datatype import get_fmin
 class CompGenerator(object):
     def __init__(self, approx1, exe1, approx2, exe2, psd = None, verbose = False):
         self._verbose = verbose
@@ -535,9 +535,10 @@ class CompGenerator(object):
             if (mode % 10) % 2 and q[i] == 1 and s1z[i] ==  s2z[i]:
                 ans = 1.
             else:
+                f_min = get_fmin(f_ini, mtotal[i])
                 ans = self._core_calcFF(m1, m2, 
                                         s1z[i], s2z[i], ecc[i],
-                                        D, f_ini, 
+                                        D, f_min, 
                                         srate, timeout, jobtag, mode = mode)
             data.append([mtotal[i], q[i], s1z[i], s2z[i], ecc[i], ans])
             sys.stderr.write(f'PMS: m1 = {m1}, m2 = {m2}, s1z = {s1z[i]}, s2z = {s2z[i]} ecc = {ecc[i]}\n\t FF = {ans}\n\n')
